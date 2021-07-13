5d4ccead63ed153dfea4eeb9a1ef060845c4d29b
Tharindu Akalaka <tharindu.thisun123@gmail.com>
2021-07-13 17:27:55 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Counter } from './features/counter/Counter';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const count = useSelector((state) => state.counter.value)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <span>{count}</span>
+        <Counter />
       </header>
     </div>
   );
